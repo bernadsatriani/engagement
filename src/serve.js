676e664677bfd92d46088b499f11a08aef71b160
Sam Garson <samtgarson@gmail.com>
2017-04-16 18:22:46 +0100

    Attribution

--- src/serve.js
+++ src/serve.js
@@ -1,6 +1,8 @@
 import { readFileSync } from 'fs'
 
-const path = './index.html'
-const html = readFileSync(path)
+const path = './src/index.html'
+const html = readFileSync(path, 'utf-8')
 
-export default async res => res.end(html)
+export default async res => {
+  res.end(html)
+}

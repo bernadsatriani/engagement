6fd6ad225f45d2b5a957186e7f6f0482674c76c7
Sam Garson <samtgarson@gmail.com>
2017-05-01 16:58:45 +0100

    Verify token for API

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import dispatch from 'micro-route/dispatch'
 import { createError } from 'micro'
 import { pipeP } from 'ramda'
+import jwt from 'jsonwebtoken'
 import getUsername from './url'
 import calculate from './calculate'
 import serve from './serve'
@@ -9,12 +10,18 @@ import auth from './auth'
 
 export const app = pipeP(async (url: string) => getUsername(url), calculate)
 
+const { SECRET_KEY: secretKey } = process.env
+
 export default dispatch()
   .dispatch('/', 'GET', serve)
   .dispatch('/error', 'GET', serve)
   .dispatch('/auth', 'GET', auth)
-  .otherwise((req: { url: string, headers: { authorization: string } }) => {
-    global.token = req.headers.authorization
-    if (global.token) return app(req.url)
-    throw createError(401, 'Authorization required')
+  .otherwise(({ headers: { authorization }, url }: { url: string, headers: { authorization: string } }) => {
+    try {
+      global.token = jwt.verify(authorization, secretKey).token
+      return app(url)
+    } catch (err) {
+      console.error(err)
+      throw createError(401, 'Authorization required')
+    }
   })

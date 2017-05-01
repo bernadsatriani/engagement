85a994b45f3d449791bf5088611ea6744dec185a
Sam Garson <samtgarson@gmail.com>
2017-05-01 17:38:16 +0100

    Secrets and e2e update

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -4,9 +4,15 @@ import { get } from 'axios'
 
 test('deployed url works', async t => {
   const { url } = await nowLatest({ token: process.env.NOW_TOKEN, name: 'engagement' })
-  const { data, status } = await get(`https://${url}/samtgarson`)
-  const result = parseFloat(data, 10)
+  try {
+    const { data, status } = await get(`https://${url}/samtgarson`, {
+      headers: { Authorization: process.env.E2E_TOKEN }
+    })
+    const result = parseFloat(data, 10)
 
-  t.is(status, 200)
-  t.is(typeof result, 'number')
+    t.is(status, 200)
+    t.is(typeof result, 'number')
+  } catch (err) {
+    t.fail(err)
+  }
 })

18e3d9de4ef1b70d82a2049ee26661818373ba1b
Sam Garson <samtgarson@gmail.com>
2017-05-01 19:53:52 +0100

    Include public_content scope

--- src/serve.js
+++ src/serve.js
@@ -1,7 +1,7 @@
 import { renderFile } from 'pug'
 
 const file = name => `./src/views/${name}.pug`
-const redirect = redirect => `https://api.instagram.com/oauth/authorize/?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=https://${redirect}/auth&response_type=code`
+const redirect = redirect => `https://api.instagram.com/oauth/authorize/?client_id=${process.env.INSTAGRAM_CLIENT_ID}&redirect_uri=https://${redirect}/auth&response_type=code&scope=basic+public_content`
 
 export default async ({ url, headers }, res, { query }, name) => {
   name = name || (url === '/' ? 'index' : url.substr(1).split('?')[0])
